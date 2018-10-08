db1a87df13656f22870aa8796f5fb100c3250e50
userand <604660729@qq.com>
2018-10-08 12:08:36 +0800

    basic

---
+++ src/button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="button">
+         <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default{
+
+
+
+    }
+
+</script>
+
+<style lang="scss">
+    .button{
+        padding: 0 1em;
+        background: pink;
+
+      &:hover{
+          background: red;
+      }  
+    }
+</style>
