1996e3f879d78dad63e38512a66e89e9b96af072
userand <604660729@qq.com>
2018-10-08 18:53:41 +0800

    完善btn组件

--- src/button.vue
+++ src/button.vue
@@ -1,25 +1,74 @@
 <template>
-    <button class="button">
-         <slot></slot>
+    <button class="g-button"   :class="{[`icon-${iconPosition}`]: true}" >
+            <svg v-if="icon"   class="icon" aria-hidden="true">
+                <use v-bind:xlink:href="`#icon-${icon}`"></use>
+            </svg>
+            <slot></slot> 
     </button>
 </template>
 
 <script>
-    export default{
-
-
-
+export default {
+    //props: ['icon','iconPosition'],
+    props: {
+        icon: {},
+        iconPosition: {
+            type: String,
+            default: 'left',
+            validator(value) {
+                return value === 'left' || value === 'right'
+            }
+        }
+    },
+    mounted() {
+        
     }
-
+}
 </script>
 
 <style lang="scss">
-    .button{
-        padding: 0 1em;
-        background: pink;
+.g-button {
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
 
-      &:hover{
-          background: red;
-      }  
+    &:hover {
+        border-color: var(--border-color-hover);
     }
+
+    &:active {
+        background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+        outline: none;
+    }
+
+    >.content {
+        order: 2;
+    }
+
+    >.icon {
+        order: 1;
+        margin-right: .1em;
+    }
+
+    &.icon-right {
+        >.content {
+            order: 1;
+        }
+
+        >.icon {
+            order: 2;
+            margin-right: 0;
+            margin-left: .1em;
+        }
+    }
+}
 </style>
